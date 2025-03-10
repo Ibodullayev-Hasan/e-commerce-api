42e39230025595ce8c7ec710cb7782aff3a5c7d6
Ibodullayev-Hasan <ibodullayevh040@gmail.com>
2025-03-10 15:32:58 +0500

    yaratilgan modullar chaqirildi

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { CategoriesModule } from './categories/categories.module';
+import { ProductsModule } from './products/products.module';
+import { BasketModule } from './basket/basket.module';
+import { OrdersModule } from './orders/orders.module';
 
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UsersModule, CategoriesModule, ProductsModule, BasketModule, OrdersModule,],
   controllers: [],
   providers: [],
 })
-export class AppModule {}
+export class AppModule { }
