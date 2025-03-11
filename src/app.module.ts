a477132636123a1a68fbd7f892dee82967f0c5f8
Ibodullayev-Hasan <ibodullayevh040@gmail.com>
2025-03-11 08:49:39 +0500

    providersda ResponseFormatterInterceptor ro'yxatdan o'tkazildi

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,11 +5,35 @@ import { CategoriesModule } from './categories/categories.module';
 import { ProductsModule } from './products/products.module';
 import { BasketModule } from './basket/basket.module';
 import { OrdersModule } from './orders/orders.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { EnvConfig, JwtConfig, TypeOrmConfig } from './configs';
+import { ConfigModule } from '@nestjs/config';
+import { APP_INTERCEPTOR } from '@nestjs/core';
+import { ResponseFormatterInterceptor } from './interceptors';
+import { JwtModule } from '@nestjs/jwt';
 
 
 @Module({
-  imports: [AuthModule, UsersModule, CategoriesModule, ProductsModule, BasketModule, OrdersModule,],
+  imports: [
+    ConfigModule.forRoot(EnvConfig),
+
+    TypeOrmModule.forRootAsync(TypeOrmConfig),
+
+    JwtModule.registerAsync(JwtConfig),
+
+    AuthModule,
+    UsersModule,
+    CategoriesModule,
+    ProductsModule,
+    BasketModule,
+    OrdersModule,
+  ],
   controllers: [],
-  providers: [],
+  providers: [
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: ResponseFormatterInterceptor
+    }
+  ],
 })
 export class AppModule { }
