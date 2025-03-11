873ad2904d6599aed49a198c5751bbcc5963f09f
Ibodullayev-Hasan <ibodullayevh040@gmail.com>
2025-03-11 08:52:39 +0500

    global perfix, NotFoundExceptionFilter qo'shildi, port .env dan olinmoqda

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { setupGlobalPipes } from './configs';
+import { NotFoundExceptionFilter } from './filters/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const configService = app.get(ConfigService)
+
+  app.setGlobalPrefix('api/v1')
+
+  setupGlobalPipes(app)
+
+  app.useGlobalFilters(new NotFoundExceptionFilter())
+
+  const port = configService.get<number>('SERVER_PORT')
+  await app.listen(port, () => {
+    console.log(`Server run on port: ${port}`)
+  });
 }
 bootstrap();
