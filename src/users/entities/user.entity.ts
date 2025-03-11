537b200ffe1f640a7826836b729b69922af22b7e
Ibodullayev-Hasan <ibodullayevh040@gmail.com>
2025-03-11 09:04:08 +0500

    user entitiysi optimizatsiya qilinyapti

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,22 @@
-export class User {}
+import { UserRole } from "src/enums";
+import { IUser } from "src/interfaces";
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity("users")
+export class User implements IUser {
+
+	@PrimaryGeneratedColumn("uuid")
+	id: string
+
+	@Column({ type: "text" })
+	full_name: string;
+
+	@Column({ type: "text", unique: true })
+	email: string;
+
+	@Column({ type: "text" })
+	password: string;
+
+	@Column({ type: "varchar", default: UserRole.USER })
+	role?: string;
+}
