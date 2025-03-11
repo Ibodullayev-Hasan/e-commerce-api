95b721fa943f2acc0b5e8793a305cc94b2ea3a9c
Ibodullayev-Hasan <ibodullayevh040@gmail.com>
2025-03-11 09:05:32 +0500

    userni yaratish authModulega register qilib ko'chirildi, moduleda authModule chaqirildi

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,16 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from 'src/entities';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    forwardRef(() => AuthModule)
+  ],
   controllers: [UsersController],
   providers: [UsersService],
 })
-export class UsersModule {}
+export class UsersModule { }
